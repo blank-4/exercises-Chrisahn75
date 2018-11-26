4e2246469fdf21f4b67a56fd69416d1aa1980c0c
Chris Ahona <chris.ahona@gmail.com>
2018-11-26 17:50:54 +0100

    exercise3

---
+++ javascript/lesson2/exercise4/exercise/index.js
@@ -0,0 +1,20 @@
+var position = "Belleville";
+
+switch (position) {
+    case  "Belleville" :
+     console.log("tu es sur la ligne 11 ou la ligne 2");
+     break;
+
+    case "Mairie des Lilas" :
+     console.log("tu es sur la ligne 11");
+     break;
+    
+    case "Père Lachaise" :
+     console.log("tu es sur la ligne 3 ou 2");
+     break;
+
+    default:
+     console.log("Je ne sais pas ou tu es.");
+     break;
+    
+}
