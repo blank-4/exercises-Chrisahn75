f42530a05913e82ef924861d751d35f304866c90
Chris Ahona <chris.ahona@gmail.com>
2018-11-26 11:57:46 +0100

    exercise 1-2

---
+++ javascript/lesson1/exercise1/exercise/index.js
@@ -0,0 +1,20 @@
+var toto="7";
+console.log (toto);
+
+toto = toto+20;
+console.log (toto);
+
+toto = toto-3;
+console.log (toto);
+
+toto= toto*4;
+console.log (toto);
+
+toto=toto/2;
+console.log (toto);
+
+toto=toto%3;
+console.log (toto);
+ 
+
+
---
+++ javascript/lesson1/exercise2/exercise/index.js
@@ -0,0 +1,8 @@
+var nb1 = 5;
+console.log(nb1);
+
+var nb2 = 4;
+console.log(nb2);
+
+var nb3 =  nb1 + nb2; 
+console.log("Le résultat de "+nb1+"+"+nb2+" est " +nb3);
