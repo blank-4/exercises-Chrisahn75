4ceac5a4c2bc9d913f466edb0544576752fb08e5
Chrisahn75 <44778648+Chrisahn75@users.noreply.github.com>
2018-11-27 12:07:50 +0100

    Javascript lesson1 (#13)
    * exercise 1-2
    
    * exercise 2

---
+++ javascript/lesson1/exercise2/exercise/index.js
@@ -0,0 +1,53 @@
+var nb1 = 5;
+console.log(nb1);
+
+var nb2 = 4;
+console.log(nb2);
+
+var nb3 =  nb1 + nb2; 
+console.log("Le résultat de "+nb1+"+"+nb2+" est " +nb3);
+
+var ub1 = 6;
+console.log(ub1);
+
+var ub2 = 3;
+console.log(ub2);
+
+var ub3 = ub1 - ub2;
+console.log("Le résultat de "+ub1+"-"+ub2+" est " +ub3);
+
+var ca1 = 8;
+console.log(ca1);
+
+var ca2 = 5;
+console.log(ca2);
+
+var ca3 = ca1 * ca2;
+console.log("Le résultat de "+ca1+"*"+ca2+" est " +ca3);
+
+var ka1 = 18;
+console.log(ka1);
+
+var ka2 = 6;
+console.log(ka2);
+
+var ka3 = ka1 / ka2;
+console.log("Le résultat de "+ka1+"/"+ka2+" est " +ka3);
+
+var mh1 = 9;
+console.log(mh1);
+
+var mh2 = 7;
+console.log(mh2);
+
+var mh3 = mh1 % mh2;
+console.log("Le résultat de "+mh1+"%"+mh2+" est " +mh3);
+
+var yb1 = "r";
+console.log(yb1);
+
+var yb2 = 7;
+console.log(yb2);
+
+var yb3 = yb1 + yb2;
+console.log("Le résultat de "+yb1+"+"+yb2+" est " +yb3);
