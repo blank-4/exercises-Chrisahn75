16cfe5394de25dda5eec1b8a2291734de2eea838
Chris Ahona <chris.ahona@gmail.com>
2018-11-29 12:21:36 +0100

    exercise 5

---
+++ javascript/lesson3/exercise5/exercise/index.js
@@ -0,0 +1,17 @@
+function circlearea (r) {
+    return Math.PI * (r *r)
+}
+
+document .write("L'air d'un cercle de rayon 3cm est " +circlearea(3) +"cm2<BR>");
+
+function circleperimeter (p) {
+    return Math.PI * (p * p)
+}
+
+document .write("Le périmetre d'un cercle de rayon 4cm est " +circleperimeter(4) +"cm<BR>");
+
+function circlediameter (p) {
+    return Math.PI * (p * p)
+}
+
+document .write("Le diametre d'un cercle de rayon 5cm est " +circlediameter(5) +"cm<BR>");
