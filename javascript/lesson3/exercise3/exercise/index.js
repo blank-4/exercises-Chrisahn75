4b52030264a47f99f036e0ba9cd55942b68b3148
Chris Ahona <chris.ahona@gmail.com>
2018-11-29 10:41:34 +0100

    exercise 3

---
+++ javascript/lesson3/exercise3/exercise/index.js
@@ -0,0 +1,8 @@
+var etage = 3;
+var porte = "droite";
+var boiteauLettres
+var monAppartement = function anonymous(boiteauLettres) {
+    console.log("Etage : " + "" +etage + "" + "Porte : " + "" +porte + "" + "Boite aux Lettres : " + "" +boiteauLettres + "" );
+}
+monAppartement ();
+
