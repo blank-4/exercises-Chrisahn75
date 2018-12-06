2e97c87a1995322ce7737638f5bb99d835e69857
Chris Ahona <chris.ahona@gmail.com>
2018-12-06 16:38:57 +0100

    exercise 1 modif

--- javascript/lesson5/exercise1/exercise/main.js
+++ javascript/lesson5/exercise1/exercise/main.js
@@ -1,34 +1,22 @@
-var y = document.getElementById("texte").innerHTML = "lorem ipsum";
+var x = document.getElementById("texte");
 
-function afficher_cacher(texte){
+x.innerHTML = "lorem ipsum";
+function afficher () {
     document.getElementById("texte").style.display = "block";
-    /* document.getElementById("texte").innerHTML = afficher (); */
-    document.getElementById('bouton_texte').innerHTML = (document.getElementById('bouton_texte').innerHTML == "Masquer le texte")? "Afficher le texte":"Masquer le texte"; 
-    var elts = document.getElementsByClassName();
-    var nbre_elts = elts.length;
-    for( var i=0; i<nbre_elts; i++) {
-      elts[i].style.visibility = (elts[i].style.visibility === "hidden" || elts[i].style.visibility === "" ) ? "visible":"hidden";
-    }
-}
-var couleurs = new Array("#AAAAAA","#F0F8FF","#8A2BE2","#4B0082","#FF0000","#0000FF","#2E8B57","#9ACD32");
- 
-function randomColor()
-{
- var n = couleurs.length;
- var c; 
- 
- c = Math.round(Math.random()*n);
- document.getElementById("texte").style.backgroundColor = couleurs[c];
- 
- c = Math.round(Math.random()*n);
- document.getElementById("texte").style.backgroundColor = couleurs[c];
- 
- c = Math.round(Math.random()*n);
- document.getElementById("texte").style.backgroundColor = couleurs[c];
- 
- c = Math.round(Math.random()*n);
- document.getElementById("texte").style.backgroundColor = couleurs[c];
- 
+    x.style.color = couleur();
+
 }
 
+function cacher (){
+    x.style.display = "none";
+}
 
+function couleur (){
+    var couleur = ["#9932CC","#20B2AA","#FF4500"];
+    var index = Math.floor (Math.random()* couleur.length);
+        console.log(index);
+        console.log(couleur);
+    var macolor = couleur[index];
+    return macolor;
+    
+}
