7b2b3046e3d0c3b787eb948334018f0449f640d5
Chrisahn75 <44778648+Chrisahn75@users.noreply.github.com>
2018-12-10 10:43:10 +0100

    Javascript-lesson2 (#14)
    * exercise 1
    
    * exercise2
    
    * exercise 3
    
    * exercise3
    
    * exercise 5

---
+++ javascript/lesson2/exercise1/exercise/main.js
@@ -0,0 +1,10 @@
+var heure = 15;
+console.log(8==8);
+
+if (heure < 10) {
+  console.log("C'est le matin");
+} else if (heure < 20){
+  console.log("C'est l'après-midi");
+} else {
+  console.log("C’est le soir");
+}
---
+++ javascript/lesson2/exercise2/exercise/index.js
@@ -0,0 +1,8 @@
+for ( var i = 0 ; i < 100 ; i++ )
+    console.log (+ i);
+
+for ( var i = 0 ; i < 100 ; i= i + 2 ) 
+    console.log (+i);
+
+for ( var i = 100 ; i > 0 ; i= i - 2 ) 
+    console.log (+i);
