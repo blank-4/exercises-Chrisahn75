b17fd78ac661f35582809b1982e50b70ce0e8956
Chrisahn75 <44778648+Chrisahn75@users.noreply.github.com>
2018-12-10 10:44:34 +0100

    javascript-lesson5 (#17)
    * exercise 1
    
    * exercise 1 modif

---
+++ javascript/lesson5/exercise1/exercise/main.js
@@ -0,0 +1,22 @@
+var x = document.getElementById("texte");
+
+x.innerHTML = "lorem ipsum";
+function afficher () {
+    document.getElementById("texte").style.display = "block";
+    x.style.color = couleur();
+
+}
+
+function cacher (){
+    x.style.display = "none";
+}
+
+function couleur (){
+    var couleur = ["#9932CC","#20B2AA","#FF4500"];
+    var index = Math.floor (Math.random()* couleur.length);
+        console.log(index);
+        console.log(couleur);
+    var macolor = couleur[index];
+    return macolor;
+    
+}
