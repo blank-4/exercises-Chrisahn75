a54dbb7b2bc0c5cfc1208c8679aa9c92c116c8e8
Chris Ahona <chris.ahona@gmail.com>
2018-12-10 14:23:32 +0100

    exercise 1 modif

--- javascript/lesson6/exercise1/exercise/main.js
+++ javascript/lesson6/exercise1/exercise/main.js
@@ -1,10 +1,13 @@
+var lheure = document.getElementById("horloge");
 function temps (){
-    var ladate=new Date()
-    document.write("Nous sommes le : ");
+    var ladate = new Date();
+    lheure.innerHTML = ladate;
+
+}
+setInterval (temps,1000);
+
+/* document.write("Nous sommes le : ");
     document.write(ladate.getDate()+"/"+(ladate.getMonth()+1)+"/"+ladate.getFullYear())
     document.write("<BR>");
-    var ladate=new Date()
     document.write("Heure : ");
-    document.write(ladate.getHours()+":"+ladate.getMinutes()+":"+ladate.getSeconds())
-}
-temps ();
+    document.write(ladate.getHours()+":"+ladate.getMinutes()+":"+ladate.getSeconds()) */
